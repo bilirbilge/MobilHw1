7aad8e9b23890bf6ae2db49272bbd3e4e7484b7a
bilirbilge <44753062+bilirbilge@users.noreply.github.com>
2018-11-04 23:17:31 +0300

    Add files via upload

---
+++ hw1ceng/Altyapı.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace hw1ceng
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Altyapı : ContentPage
+    {
+
+
+        public Altyapı()
+        {
+            InitializeComponent();
+            var listView = new ListView();
+            listView.ItemsSource = new string[]
+            {
+  "Derslik-B1",
+  "Derslik-B2",
+  "Derslik-B3",
+  "Derslik-B4",
+  "Derslik-B5",
+  "Derslik-B6",
+  "Derslik-B4",
+  "Lab-01",
+  "Lab-02"
+            };
+        }
+
+
+    }
+}
+
+    
+
